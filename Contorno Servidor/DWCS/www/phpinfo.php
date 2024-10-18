d9958e7e5ffd405526bee2fc5b57e663e1c6c529
A23BeatrizFP <a23beatrizfp@a24eql01.sanclemente.local>
2024-10-18 11:04:21 +0200

    x

--- Contorno Servidor/DWCS/www/phpinfo.php
+++ Contorno Servidor/DWCS/www/phpinfo.php
@@ -2,4 +2,16 @@
 
 phpinfo();
 
-?>
+    
+
+
+
+$valores = [1,5,7,9];
+
+for($i = 0; $i < count($valores);$i++){
+    if($i == $_GET['posicion']){
+        echo $valores[$i];
+    }
+}
+
+echo $valores[$_GET['posicion']];
