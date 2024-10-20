f21a56da5e52114154f0cf5cf5536b52d8f89853
Bea <beatrizfontanpadin@hotmail.com>
2024-10-20 13:10:51 +0200

    x

--- Contorno Cliente/2_Iniciación a Javascript/9 - Estructuras repetitivas/Fontán_Padín_Beatriz - Estructuras repetitivas.js
+++ Contorno Cliente/2_Iniciación a Javascript/9 - Estructuras repetitivas/Fontán_Padín_Beatriz - Estructuras repetitivas.js
@@ -4,44 +4,55 @@
 //Bucle for
 
 let texto = prompt("Dime un texto"); // Variable para recoger el texto que nos introduce el usuario
-let resultado = ''; // Variable para almacenar el texto sin vocales
+let resultado = ''; // variable para recoger el texto transformado
 
 // Bucle para recorrer el texto
 for (let i = 0; i < texto.length; i++) {
-    // Si el carácter no es una vocal
-    if (texto[i] != "a" && texto[i] != "e" && texto[i] != "i" && texto[i] != "o" && texto[i] != "u") {
+
+    if (texto[i] != "a" && texto[i] != "e" && texto[i] != "i" && texto[i] != "o" && texto[i] != "u") {  // Si el carácter no es una vocal
         resultado += texto[i];//Lo añadimos a la variable de resultado
     }
 }
 
-// Mostrar el texto modificado
-alert(resultado);
+alert(resultado);//Mostramos el resultado
 
 
 
 //Bucle Do while
 
 let texto2 = prompt("Dime un texto"); // Recoge el texto introducido
-let resultado2 = ''; // Variable para almacenar el texto sin vocales
-i = 0; // Inicializamos la variable de control
+let resultado2 = ''; //variable para recoger el texto transformado
+i = 0; // reiniciamos el valor de i para volver a utilizarlo
 
 // Bucle para recorrer el texto
 do {
     // Si el carácter no es una vocal, lo añadimos al resultado
     if (texto2[i] != "a" && texto2[i] != "e" && texto2[i] != "i" && texto2[i] != "o" && texto2[i] != "u") {
-        resultado2 += texto2[i];
+        resultado2 += texto2[i];//Lo añadimos a la variable de resultado
     }
-    i++;
-} while (i < texto2.length); // Condición para continuar recorriendo el texto
+    i++;//y aumentamos la variable de control para seguir recorriendo el texto
+} while (i < texto2.length);// Condición para continuar recorriendo el texto
 
-// Mostrar el texto modificado
-alert(resultado2);
-
-
-//Bucle while
-
-let texto3;
-let resultado3;
+alert(resultado2);//Mostramos el resultado
 
 
 
+//Bucle while y continue
+
+let texto3 = prompt("Dime un texto");//recogemos el texto que nos introduce el usuario
+let resultado3 = '';//variable para almacenar el texto transformado
+i = 0; //Reiniciamos el valor de variable de control para volver a utilizarla
+
+while (i < texto3.length) {
+    // Si el texto contine una vocal 
+    if ("aeiou".includes(texto3[i])) {
+        i++;//aumenta la variable de control
+        continue; //y continua con la siguiente comprobación
+    }
+
+    // Si encuentra una vocal
+    resultado3 += texto3[i];//se añade a la variable de transformación
+    i++;//aumentamos la variable de control
+}
+
+alert(resultado3);//Mostramos el resultado
