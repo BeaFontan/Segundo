0ebbf7706e8fadd41b9638e3127e767224879514
Beatriz Fontán <beatriz@squareetlabs.com>
2024-12-12 17:02:49 +0100

    x

---
+++ Contorno Cliente/zExamen/21_Tipos_Examen/2_Tipo Examen 2/ej2.js
@@ -0,0 +1,19 @@
+// 2. Escribir una función llamada transcribir que reciba un string (una cadena de ADN) y 
+// retorne otro string (su complemento ARN).
+//  Los complementos son los siguientes:
+//  • G -> C 
+// • C -> G 
+// • T -> A 
+// • A -> U 
+
+// escribe tu función acá
+
+
+
+
+
+
+
+ // código de prueba
+ console.log(transcribir("ACGT")) // "UGCA"
+ console.log(transcribir("ACGTGGTCTTAA")) // "UGCACCAGAAUU"
---
+++ Contorno Cliente/zExamen/21_Tipos_Examen/2_Tipo Examen 2/ej5.js
@@ -0,0 +1,9 @@
+// 5. Tu tarea es ordenar una cadena determinada. Cada palabra de la cadena contendrá un único 
+// número. Este número es la posición que debe tener la palabra en el resultado.
+//  Nota: Los números pueden ser del 1 al 9. Por lo tanto, 1 será la primera palabra (no 0).
+//  Si la cadena de entrada está vacía, devuelve una cadena vacía. Las palabras en la cadena de entrada 
+// solo contendrán números consecutivos válidos.
+
+//  Ejemplos
+//  "este2 e1s T4est u3n"  -->  "Est1e es2 u3n T4est"
+//  "4de pa1ra ge
